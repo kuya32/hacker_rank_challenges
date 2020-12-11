7a371fdcb05865cbe805173a1ce26db8c157bc58
Marchael Acode <m.acode@outlook.com>
2020-12-11 12:08:16 -0800

    Created divisibleSumPairs function

--- src/main/java/hacker_rank_challenges/easyChallenges.java
+++ src/main/java/hacker_rank_challenges/easyChallenges.java
@@ -244,4 +244,19 @@ public class easyChallenges {
         output[1] = counterLowest;
         return output;
     }
+
+    static int divisibleSumPairs(int n, int k, int[] ar) {
+        int counter = 0;
+        for (int i = 0; i < ar.length; i++) {
+            for (int j = i + 1; j < ar.length; j++) {
+                if ((ar[i] + ar[j]) % k == 0) {
+                    counter++;
+                }
+            }
+        }
+        return counter;
+    }
+
+    
+
 }
