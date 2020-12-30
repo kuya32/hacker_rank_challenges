debba2a4539263cd0d95ea3a941e2c47db73c732
Marchael Acode <m.acode@outlook.com>
2020-12-29 23:20:49 -0800

    Created a function to print out the numbers on the top view of a binary tree

--- src/main/java/hacker_rank_challenges/easyChallenges.java
+++ src/main/java/hacker_rank_challenges/easyChallenges.java
@@ -810,6 +810,27 @@ public class easyChallenges {
         return left > right ? left:right;
     }
 
+    public static void topView(Node root) {
+        leftView(root.left);
+        System.out.print(root.data + " ");
+        rightView(root.right);
+    }
+
+    public static void leftView(Node root) {
+        if (root == null) return;
+        leftView(root.left);
+        System.out.print(root.data + " ");
+    }
+
+    public static void rightView(Node root) {
+        if (root == null) return;
+        System.out.print(root.data + " ");
+        rightView(root.right);
+
+    }
+
+    
+
 
     public static class Node {
         int data;
